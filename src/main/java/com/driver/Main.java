e6019df6783b8048574fbc41fcacddfb1d70b3e8
dudenehal25 <dudenehal25@gmail.com>
2022-10-12 12:33:05 +0530

    completed and tested

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,7 +2,11 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
+
         B obj = new B();
+
+
+        System.out.println(obj.meth2());
         System.out.println(obj.meth());
     }
 
@@ -19,4 +23,9 @@ class B extends A{
     public String meth() {
         return "Invoking method from class B";
     }
+
+
+    public String meth2() {
+        return super.meth();
+}
 }
